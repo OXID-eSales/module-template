f4c847b8304a1082b31c1c4eda1dcd45de0debc5
hkreuter <heike.reuter@oxid-esales.com>
2022-03-25 10:04:17 +0100

    OXDEV-5165 Fix test namespace

--- tests/Integration/Controller/GreetingControllerTest.php
+++ tests/Integration/Controller/GreetingControllerTest.php
@@ -7,7 +7,7 @@
 
 declare(strict_types=1);
 
-namespace OxidEsales\ModuleTemplate\Tests\Integration\Service;
+namespace OxidEsales\ModuleTemplate\Tests\Integration\Controller;
 
 use OxidEsales\Eshop\Application\Model\User as EshopModelUser;
 use OxidEsales\ModuleTemplate\Controller\GreetingController;
