b4e5df30e1438f9f08dce0dc86467e35bf9b4459
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-04-06 16:45:30 +0300

    OXDEV-5165 Update personalize bash script and readme

--- bin/personalize.sh
+++ bin/personalize.sh
@@ -3,54 +3,78 @@
 SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
 cd $SCRIPT_PATH/.. || exit
 
-projectname_original='oxid-esales/module-template'
-vendor_original='OxidEsales'
-module_original='ModuleTemplate'
-moduleid_original='oe_moduletemplate'
-company_original='OXID eSales AG'
-metadata_title_original='OxidEsales Module Template'
-namespace_original='OxidEsales\\ModuleTemplate'
-target_directory_original='oe/moduletemplate'
+package_name='oxid-esales/module-template'
+namespace='OxidEsales\ModuleTemplate'
+module_id='oe_moduletemplate'
+company='OXID eSales AG'
+target_directory='oe/moduletemplate'
 
 echo
 echo "In order to convert this module template to your own, you will be asked for some information."
 echo
-echo "Please enter project name (original: $projectname_original):"
-read projectname
-perl -pi -e "s#$projectname_original#$projectname#g;"  ./composer.json
+echo "Please enter package name (original: $package_name):"
+read -r package_name_input
+sed -ri "s#$package_name#$package_name_input#g;" ./composer.json
+sed -ri "s#$package_name#$package_name_input#g;" ./.github/workflows/development.yml
 echo
 
-echo "Please enter vendor (original: $vendor_original):"
-read vendor
-perl -pi -e "s#$vendor_original#$vendor#g;"  ./composer.json
+echo "Please enter module namespace (original: $namespace):"
+# Prepare original namespace for replacement in composer.json file
+namespace=$(echo "$namespace" | sed -r 's#\\#\\\\\\\\#g')
+read -r namespace_input
+# Extract vendor name from provided namespace, will be need for later use
+vendor_name=$(echo "$namespace_input" | sed 's#\([^\\]*\)\\.*#\1#g')
+# Extract module name from provided namespace, will be need for later use
+module_name=$(echo "$namespace_input" | sed 's#.*\\\(.*\)#\1#g')
+# Prepare provided namespace for replacement in composer.json file
+namespace_input=$(echo "$namespace_input" | sed -r 's#\\{1,}#\\\\\\\\#g')
+sed -ri "s#$namespace#$namespace_input#g;" ./composer.json
+# Prepare original namespace for replacement in the module files by handling \ counts
+namespace=$(echo "$namespace" | sed -r 's#\\\\\\\\#\\\\#g')
+# Prepare input namespace for replacement in the module files by handling \ counts
+namespace_input=$(echo "$namespace_input" | sed -r 's#\\\\\\\\#\\\\#g')
+find . -type f \( ! -name "personalize.sh" -and ! -name "README.md" \) -exec grep -l "$namespace" {} \; |xargs perl -pi -e "s#$namespace#$namespace_input#g;"
 echo
 
-echo "Please enter module namespace (original: $module_original):"
-read module
-perl -pi -e "s#$module_original#$module#g;"  ./composer.json
+# Compose module id based on <yourVendorPrefix> and <yourModuleRootDirectory> directories
+module_root_directory=${PWD##*/}
+vendor_directory_path=$(echo "$PWD" | sed -r "s#$module_root_directory##g")
+vendor_prefix="${vendor_directory_path%"${vendor_directory_path##*[!/]}"}"
+vendor_prefix="${vendor_prefix##*/}"
+composed_module_id="${vendor_prefix}_${module_root_directory}"
+echo "Your module id is: '$composed_module_id'!"
 echo
 
-echo "Please enter module id (original: $moduleid_original)"
-read moduleid
-find . -type f \( ! -name "personalize.sh" \) -exec grep -l "$moduleid_original" {} \; |xargs perl -pi -e "s#$moduleid_original#$moduleid#g;"
-perl -pi -e "s#$company_original#$vendor#g;"  ./metadata.php
-perl -pi -e "s#$metadata_title_original#CHANGE MY TITLE#g;"  ./metadata.php
-echo
+# Replace module id everywhere except in this file
+find . -type f \( ! -name "personalize.sh" -and ! -name "README.md" \) -exec grep -l "$module_id" {} \; |xargs perl -pi -e "s#$module_id#$composed_module_id#g;"
+
+# Change title in metadata.php file
+perl -pi -e "s#OxidEsales Module Template \(OETM\)#CHANGE MY TITLE#g;" ./metadata.php
 
 #File headers
-echo "Please enter company name (original: $company_original)"
-read company
-find . -type f \( ! -name "personalize.sh" \) -exec grep -l "$company_original" {} \; |xargs perl -pi -e "s#$company_original#$company#g;"
+echo "Please enter company name (original: $company)"
+read company_input
+find . -type f \( ! -name "personalize.sh" \) -exec grep -l "$company" {} \; |xargs perl -pi -e "s#$company#$company_input#g;"
 
 #target directory
-target_directory="$(echo $vendor | tr '[:upper:]' '[:lower:]')/$(echo $module | tr '[:upper:]' '[:lower:]')"
-perl -pi -e "s#$target_directory_original#$target_directory#g;"  ./composer.json
-perl -pi -e "s#$target_directory_original#$target_directory#g;"  ./metadata.php
-perl -pi -e "s#$target_directory_original#$target_directory#g;"  ./tests/Codeception/acceptance.suite.yml
+composed_target_directory="${vendor_prefix}/${module_root_directory}"
+perl -pi -e "s#$target_directory#$composed_target_directory#g;" ./composer.json
+perl -pi -e "s#$target_directory#$composed_target_directory#g;" ./metadata.php
+perl -pi -e "s#$target_directory#$composed_target_directory#g;" ./tests/Codeception/acceptance.suite.yml
+perl -pi -e "s#$target_directory#$composed_target_directory#g;" ./.github/workflows/development.yml
 
-#namespace
-namespace="${vendor}\\\\${module}"
-find . -type f \( ! -name "personalize.sh" \) -exec grep -l "$namespace_original" {} \; |xargs perl -pi -e "s#$namespace_original#$namespace#g;"
+# Prepare ./.github/workflows/development.yml file
+perl -pi -e "s#SONARCLOUD_ORGANIZATION: 'oxid-esales'#SONARCLOUD_ORGANIZATION: 'CHANGE SONARCLOUD ORGANIZATION'#g;" ./.github/workflows/development.yml
+perl -pi -e "s#SONARCLOUD_PROJECT_KEY: 'OXID-eSales_module-template'#SONARCLOUD_PROJECT_KEY: 'CHANGE SONARCLOUD PROJECT KEY'#g;" ./.github/workflows/development.yml
+
+# Prepare ./migration/migrations.yml file
+perl -pi -e "s#name: OXID Module Template#name: $vendor_name $module_name#g;" ./migration/migrations.yml
+
+# Prepare ./CHANGELOG.md file
+perl -pi -e "s#OXID eShop Module Template#$vendor_name $module_name#g;" ./CHANGELOG.md
+perl -0777pi\
+  -e 's#(^.*?\#\# \[1\.0\.0\] - Unreleased)(.*?)$#\1#gs'\
+  ./CHANGELOG.md
 
 echo
-echo 'Please commit the changes. Your module is now ready to go and be adapted to your needs.'
+echo 'Please review and commit the changes. Your module is now ready to go and be adapted to your needs.'
